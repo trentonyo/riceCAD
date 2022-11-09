4c535d9f22aeb17a526f6df951d7e00ef67e506c
TrentonYo <trentonyo@gmail.com>
2022-11-08 23:16:11 -0800

    Cleaned up some semicolons and warnings

---
+++ public_html/materials.js
@@ -0,0 +1,101 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var blackCode = 20; //oops
+var blackMaterial = new pc.PhongMaterial();
+blackMaterial.diffuse.set(0.11, 0.11, 0.13);
+blackMaterial.opacity = 0.5;
+blackMaterial.update();
+
+var redCode = 1;
+var redMaterial = new pc.PhongMaterial();
+redMaterial.diffuse.set(0.69, 0.18, 0.15);
+redMaterial.opacity = 0.5;
+redMaterial.update();
+
+var greenCode = 2;
+var greenMaterial = new pc.PhongMaterial();
+greenMaterial.diffuse.set(0.37, 0.49, 0.09);
+greenMaterial.opacity = 0.5;
+greenMaterial.update();
+
+var brownCode = 3;
+var brownMaterial = new pc.PhongMaterial();
+brownMaterial.diffuse.set(0.51, 0.33, 0.2);
+brownMaterial.opacity = 0.5;
+brownMaterial.update();
+
+var blueCode = 4;
+var blueMaterial = new pc.PhongMaterial();
+blueMaterial.diffuse.set(0.24, 0.27, 0.67);
+blueMaterial.opacity = 0.5;
+blueMaterial.update();
+
+var purpleCode = 5;
+var purpleMaterial = new pc.PhongMaterial();
+purpleMaterial.diffuse.set(0.54, 0.2, 0.72);
+purpleMaterial.opacity = 0.5;
+purpleMaterial.update();
+
+var cyanCode = 6;
+var cyanMaterial = new pc.PhongMaterial();
+cyanMaterial.diffuse.set(0.09, 0.61, 0.61);
+cyanMaterial.opacity = 0.5;
+cyanMaterial.update();
+
+var lightGrayCode = 7;
+var lightGrayMaterial = new pc.PhongMaterial();
+lightGrayMaterial.diffuse.set(0.62, 0.62, 0.59);
+lightGrayMaterial.opacity = 0.5;
+lightGrayMaterial.update();
+
+var grayCode = 8;
+var grayMaterial = new pc.PhongMaterial();
+grayMaterial.diffuse.set(0.28, 0.31, 0.32);
+grayMaterial.opacity = 0.5;
+grayMaterial.update();
+
+var pinkCode = 9;
+var pinkMaterial = new pc.PhongMaterial();
+pinkMaterial.diffuse.set(0.95, 0.55, 0.67);
+pinkMaterial.opacity = 0.5;
+pinkMaterial.update();
+
+var limeCode = 10;
+var limeMaterial = new pc.PhongMaterial();
+limeMaterial.diffuse.set(0.5, 0.78, 0.12);
+limeMaterial.opacity = 0.5;
+limeMaterial.update();
+
+var yellowCode = 11;
+var yellowMaterial = new pc.PhongMaterial();
+yellowMaterial.diffuse.set(0.99, 0.85, 0.24);
+yellowMaterial.opacity = 0.5;
+yellowMaterial.update();
+
+var lightBlueCode = 12;
+var lightBlueMaterial = new pc.PhongMaterial();
+lightBlueMaterial.diffuse.set(0.23, 0.7, 0.85);
+lightBlueMaterial.opacity = 0.5;
+lightBlueMaterial.update();
+
+var magentaCode = 13;
+var magentaMaterial = new pc.PhongMaterial();
+magentaMaterial.diffuse.set(0.78, 0.31, 0.74);
+magentaMaterial.opacity = 0.5;
+magentaMaterial.update();
+
+var orangeCode = 14;
+var orangeMaterial = new pc.PhongMaterial();
+orangeMaterial.diffuse.set(0.97, 0.5, 0.11);
+orangeMaterial.opacity = 0.5;
+orangeMaterial.update();
+
+var whiteCode = 15;
+var whiteMaterial = new pc.PhongMaterial();
+whiteMaterial.diffuse.set(0.98, 1.0, 1.0);
+whiteMaterial.opacity = 0.5;
+whiteMaterial.update();
