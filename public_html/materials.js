eaad6ce62e6f8ba2b346c4440ee4e838c31dfc0b
TrentonYo <trentonyo@gmail.com>
2022-11-09 01:05:45 -0800

    Removed legacy materials

--- public_html/materials.js
+++ public_html/materials.js
@@ -41,7 +41,7 @@ Material.prototype.updateColor = function(color)
 }
 Material.prototype.getPCColor = function ()
 {
-    return this.phong.diffuse
+    return this.getPhongMaterial().diffuse
 }
 
 let deleteMe = function (r, g, b, color)
@@ -54,66 +54,34 @@ let deleteMe = function (r, g, b, color)
     console.log(color, output)
 }
 
-let blackCode = 20
 let minecraftBlack = new Material("Minecraft Black", "#1c1c21", false)
-let blackMaterial = minecraftBlack.getPhongMaterial()
 
-let redCode = 1
 let minecraftRed = new Material("Minecraft Red", "#af2d26", false)
-let redMaterial = minecraftRed.getPhongMaterial()
 
-let greenCode = 2
 let minecraftGreen = new Material("Minecraft Green", "#5e7c16", false)
-let greenMaterial = minecraftGreen.getPhongMaterial()
 
-let brownCode = 3
 let minecraftBrown = new Material("Minecraft Brown", "#825433", false)
-let brownMaterial = minecraftBrown.getPhongMaterial()
 
-let blueCode = 4
 let minecraftBlue = new Material("Minecraft Blue", "#3d44aa", false)
-let blueMaterial = minecraftBlue.getPhongMaterial()
 
-let purpleCode = 5
 let minecraftPurple = new Material("Minecraft Purple", "#8933b7", false)
-let purpleMaterial = minecraftPurple.getPhongMaterial()
 
-let cyanCode = 6
 let minecraftCyan = new Material("Minecraft Cyan", "#169b9b", false)
-let cyanMaterial = minecraftCyan.getPhongMaterial()
 
-let lightGrayCode = 7
 let minecraftLightGray = new Material("Minecraft Cyan", "#9e9e96", false)
-let lightGrayMaterial = minecraftLightGray.getPhongMaterial()
 
-let grayCode = 8
 let minecraftGray = new Material("Minecraft Gray", "#474f51", false)
-let grayMaterial = minecraftGray.getPhongMaterial()
 
-let pinkCode = 9
 let minecraftPink = new Material("Minecraft Pink", "#f28caa", false)
-let pinkMaterial = minecraftPink.getPhongMaterial()
 
-let limeCode = 10
 let minecraftLime = new Material("Minecraft Lime", "#7fc61e", false)
-let limeMaterial = minecraftLime.getPhongMaterial()
 
-let yellowCode = 11
 let minecraftYellow = new Material("Minecraft Yellow", "#fcd83d", false)
-let yellowMaterial = minecraftYellow.getPhongMaterial()
 
-let lightBlueCode = 12
 let minecraftLightBlue = new Material("Minecraft Light Blue", "#3ab2d8", false)
-let lightBlueMaterial = minecraftLightBlue.getPhongMaterial()
 
-let magentaCode = 13
 let minecraftMagenta = new Material("Minecraft Magenta", "#c64fbc", false)
-let magentaMaterial = minecraftMagenta.getPhongMaterial()
 
-let orangeCode = 14
 let minecraftOrange = new Material("Minecraft Orange", "#f77f1c", false)
-let orangeMaterial = minecraftOrange.getPhongMaterial()
 
-let whiteCode = 15
 let minecraftWhite = new Material("Minecraft White", "#f9ffff", false)
-let whiteMaterial = minecraftWhite.getPhongMaterial()
