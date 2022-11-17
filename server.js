4fda289afeeb01ff2feda34649457ad55f2ac830
Trenton Young <trentonyo@gmail.com>
2022-11-17 14:40:10 -0800

    Cleaned up and commented server script

--- server.js
+++ server.js
@@ -10,45 +10,64 @@ let app = express()
 
 let serveHomepage = function (req, res, next)
 {
-    console.log("SERVER: GET Request received (HOME PAGE)")
-    console.log("--  URL", req.url)
-
     res.status(200).sendFile(__dirname+"/public/index.html")
 }
 
+/**
+ * Log request information at the top of any request
+ */
+app.use(function (req, res, next)
+{
+    console.log("SERVER: GET Request received")
+    console.log("--  METHOD", req.method)
+    console.log("--  URL", req.url)
+    next()
+})
+
+/**
+ * Serve homepages from many different URLs
+ */
 app.get("/", serveHomepage)
 app.get("/home", serveHomepage)
 app.get("/index", serveHomepage)
 
+/**
+ * Serve library files
+ */
 app.get("/lib/*", function (req, res, next)
 {
-    console.log("SERVER: GET Request received (lib/)")
-    console.log("--  URL", req.url)
-
     res.status(200).sendFile(__dirname+req.url)
 })
 
+/**
+ * Serve main css with shorthand
+ */
 app.get("/main.css", function (req, res, next)
 {
-    console.log("SERVER: GET Request received (main.css)")
-    console.log("--  URL", req.url)
-
     res.status(200).sendFile(__dirname+"/public/main.css")
 })
 
+/**
+ * Serve playcanvas source without giving out internal path
+ */
 app.get("/playcanvas.js", function (req, res, next)
 {
-    console.log("SERVER: GET Request received (playcanvas.js)")
-    console.log("--  URL", req.url)
-
     res.status(200).sendFile(__dirname+"/node_modules/playcanvas/build/playcanvas.js")
 })
 
+/**
+ * Serve FileSaver source without giving out internal path
+ */
+app.get("/filesaver.js", function (req, res, next)
+{
+    res.status(200).sendFile(__dirname+"/node_modules/playcanvas/build/filesaver.js")
+})
+
+/**
+ * Handle project pages
+ */
 app.get("/project/:projectID", function (req, res, next)
 {
-    console.log("SERVER: GET Request received (PROJECT)")
-    console.log("--  URL", req.url)
-
     let content = "<html><body>"
     content += "<h1>Info about project with ID <span style='color: darkblue'>"
     content += req.params.projectID
@@ -57,11 +76,11 @@ app.get("/project/:projectID", function (req, res, next)
     res.status(200).send(content)
 })
 
-app.get("*", function (req, res, next) //Star is wildcard
+/**
+ * 404 - final fallthrough reached
+ */
+app.get("*", function (req, res, next)
 {
-    console.log("SERVER: GET Request received (UNROUTED)")
-    console.log("--  URL", req.url)
-
     res.status(404).sendFile(__dirname+"/public/404.html")
 })
 
