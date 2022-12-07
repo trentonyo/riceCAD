090929b12dff72e2206465b5f3e2c26fcbb385f2
CameronKroeker47 <cameron.kroeker@gmail.com>
2022-12-06 17:39:43 -0800

    prints out checked values in log

--- public/Filter.js
+++ public/Filter.js
@@ -4,26 +4,30 @@ function filterPosts(){
     var posts = document.getElementsByClassName("post")
     console.log(posts.length)
 
-    var filtertag = document.getElementById("ftag").value
+    var filtertag1 = document.getElementById("filter_tag_prototype").checked
+    var filtertag2 = document.getElementById("filter_tag_nordic").checked
+    var filtertag3 = document.getElementById("filter_tag_futuristic").checked
+    
     var filterName = document.getElementById("fname").value
     var filterMin = document.getElementById("fdownloads_min").value
     var filterMax = document.getElementById("fdownloads_max").value
 
 
-    if (checkEmpty(filtertag,filterMin,filterMax,filterName) == false) {logfilterValues(filtertag,filterMin,filterMax,filterName)}
-
-
+    if (checkEmpty(filtertag1, filtertag2, filtertag3, filterMin,filterMax,filterName) == false) {logfilterValues(filtertag1, filtertag2, filtertag3,filterMin,filterMax,filterName)}
     
 }
 
-function checkEmpty (tag, min, max, name, author){
-    if (tag =='' && name =='' && min=='' && max==''){console.log ("All Empty"); return true}
+function checkEmpty (tag1, tag2, tag3, min, max, name, author){
+    if (tag1 ==false && tag2 ==false && tag3 ==false && name =='' && min=='' && max==''){console.log ("All Empty"); return true}
     else return false;
 
 }
 
-function logfilterValues(tag, min, max, name){
-console.log("filtertag ==", tag)
+function logfilterValues(tag1, tag2, tag3, min, max, name){
+console.log("filtertag ==", tag1)
+console.log("filtertag ==", tag2)
+console.log("filtertag ==", tag3)
+
 console.log ("filterName ==", name)
 console.log ("filterMin ==", min)
 console.log ("filterMax ==", max)
