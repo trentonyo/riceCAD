08b46ebb8be6dae986644151092ba0721f4f3ed5
TrentonYo <trentonyo@gmail.com>
2022-12-09 10:23:39 -0800

    Added version number at 404, incremented it for grins

--- server.js
+++ server.js
@@ -599,7 +599,9 @@ app.get("/projects/:projectID", serveProjectPage)
  */
 app.get("*", function (req, res, next)
 {
-    res.status(404).render("pageNotFound", {})
+    res.status(404).render("pageNotFound", {
+        "toolVersion" : packageJSON.version
+    })
 })
 
 app.listen(port, undefined,function ()
