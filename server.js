fc409137f32f769d7cf862717794b28754bbbaa5
Trenton Young <trentonyo@gmail.com>
2023-06-22 14:28:08 -0700

    Removed unnecessary prints

--- server.js
+++ server.js
@@ -114,14 +114,11 @@ let incrementDownloads = function(projectID)        // TODO refactor to UPDATE t
 {
     db.pool.query(`SELECT downloads FROM public.projects WHERE project_id='${projectID}';`, function (err, results, fields)
     {
-        console.log("SELECT downloads results:", results)
         let incremented_downloads = results.rows[0].downloads
         incremented_downloads++
-        console.log("Which makes downloads:", incremented_downloads)
 
         db.pool.query(`UPDATE public.projects SET downloads = ${incremented_downloads} WHERE project_id='${projectID}';`, function (err, results, fields)
-        // db.pool.query(`-- UPDATE public.projects SET downloads = 69 WHERE project_id='${projectID}';`, function (err, results, fields)
-        {
+       {
             if(err)
             {
                 console.log(err)
@@ -129,7 +126,7 @@ let incrementDownloads = function(projectID)        // TODO refactor to UPDATE t
         })
     })
 
-    if (!(projectID in projectMetaDataJSON))
+    if (!(projectID in projectMetaDataJSON)) //TODO remove after refactor
     {
         console.log("Attempting to increment downloads for invalid project ID.")
         return
